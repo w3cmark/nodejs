066acabc39e9d3594afefde81e768a98d6341327
grp.wzj@corp.netease.com <grp.wzj@corp.netease.com>
2016-01-31 23:06:35 +0800

    install express jade

--- node_blog/app.js
+++ node_blog/app.js
@@ -2,12 +2,15 @@ var express = require('express'),
     app = express(),
     port = process.env.PORT || 3000;
 
-// app.set('views', './views');
+app.set('views', './views');
+app.set('view engine', 'jade');
 app.listen(port);
 console.log('w3cmark started on port '+ port);
 
 // idnex page
 
 app.get('/',function(req, res){
-    res.render('index')
+    res.render('index',{
+    	title: '博客首页'
+    })
 })
