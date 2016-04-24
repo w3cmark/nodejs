2a35b351142143bf375733801d57a2a0b280cd10
grp.wzj@corp.netease.com <grp.wzj@corp.netease.com>
2016-04-24 21:39:11 +0800

    增加 node-img

---
+++ node-img/app.js
@@ -0,0 +1,118 @@
+var fs = require('fs'),
+	express = require('express'),
+	bodyParser = require('body-parser'),
+	path =  require('path'),
+	app = express(),
+	port = process.env.POTR || 3000,
+	gm = require('gm');
+ 
+ app.set('views', './views')
+ app.set('view engine', 'jade')
+ app.use(bodyParser.urlencoded())
+ app.use(express.static(path.join(__dirname, 'public')))
+ app.listen(port)
+
+ console.log('start on port '+ port)
+
+
+// index page
+app.get('/', function(req, res){
+
+	res.render('index',{
+		title: '首页',
+		config: {
+			inputdir: ' ',
+			outputdir: ' ',
+			size: ' ',
+			startpoint: ' ',
+			space: ' '
+		}
+	})
+	
+})
+
+// post
+app.get('/crop', function(req, res){
+	var config = req.query,
+		inputPath = config.inputdir ? config.inputdir : 'img',
+		outputPath = config.outputdir ? config.outputdir : 'output',
+		size = config.size,
+		startpoint = config.startpoint ? config.startpoint : '0, 0',
+		space = config.space ? config.space : '0, 0';
+	
+	readFiles(inputPath, outputPath, size, startpoint, space, function(data){
+		res.send(config.callback+'('+ JSON.stringify(data) +')');
+	});
+	// res.send(config.callback+'({success: 1})');
+})
+
+// 遍历所有图片
+function readFiles(inputPath, outputPath, size, startpoint, space, callback){
+	fs.readdir(inputPath, function(err, files){
+		if(!err){
+			var total = 0,
+				sucnum = 0,
+				errnum = 0;
+			for (var i = 0, len = files.length; i < len; i++) {
+				var inputfile = inputPath + '/' + files[i],
+					outputfile = outputPath + '/' + files[i],
+					mess = '';
+				console.log(inputfile);
+				if (!fs.existsSync(outputPath)) {
+		            fs.mkdirSync(outputPath);
+		            console.log(outputPath + '目录创建成功');
+		        }
+				gmCrop(inputfile, outputfile, size, startpoint, space, i, function(err, f1, f2){
+					total++;
+					if (!err){
+						sucnum++;
+						mess += f1 + ' is crop success! Save as ' + f2 + '!<br/>';
+					}else{
+						errnum++;
+						mess += err+ '<br/>';
+					}
+					if(total == len){
+						callback({success: 1, mess: mess, suc: sucnum, err: errnum});
+					}
+				})
+			};
+			
+		}else{
+			console.log(err);
+		}
+	})
+}
+
+
+// resize and remove EXIF profile data 
+function gmCrop(inputfile, outputfile, size, startpoint, space, cur, callback){
+
+	var w = dismantle(size)[0],
+		h = dismantle(size)[1],
+		x_space = dismantle(space)[0],
+		y_space = dismantle(space)[1],
+		x = dismantle(startpoint)[0] + cur*x_space,
+		y = dismantle(startpoint)[1] + cur*y_space;
+
+	gm(inputfile)
+	.crop(w, h, x , y)
+	.noProfile()
+	.write(outputfile, function(err){
+		if(typeof callback == 'function'){
+			callback(err, inputfile, outputfile)
+		}
+	});
+}
+
+//dismantle
+function dismantle(arr){
+	var newArr
+	if(arr.indexOf(',') <= -1){
+		newArr = [arr, arr];
+	}else{
+		arr = arr.split(',');
+		newArr = [arr[0], arr[1]];
+	}
+	return newArr;
+}
+
---
+++ node-img/public/js/index.js
@@ -0,0 +1,67 @@
+$('#Jbtn').bind('click', function(){
+	$('#Jresult').html('');
+	var $cropform = $('#Jcropform'),
+		inputdir = $cropform.find('input[name="config[inputdir]"]').val(),
+		outputdir = $cropform.find('input[name="config[outputdir]"]').val(),
+		size = $cropform.find('input[name="config[size]"]').val(),
+		startpoint = $cropform.find('input[name="config[startpoint]"]').val(),
+		space = $cropform.find('input[name="config[space]"]').val();
+
+	if(space && !checkInput(space)){
+		alert('请填写正确的坐标递增值！');
+		return false;
+	}
+	if(startpoint && !checkInput(startpoint)){
+		alert('请填写正确的起点坐标！');
+		return false;
+	}
+	if(!checkInput(size)){
+		alert('请填写正确的裁剪尺寸！');
+		return false;
+	}
+	
+	// if(!checkInput(startpoint)){
+	// 	alert('请填写正确的裁剪起点坐标！');
+	// 	return false;
+	// }
+	$('#Jresult').prepend('<p>正在裁剪...</p>');
+
+	$.ajax({
+		type: 'get',
+		url: 'crop',
+		dataType: 'jsonp',
+		data: {
+			'inputdir': inputdir,
+			'outputdir': outputdir,
+			'size': size,
+			'startpoint': startpoint,
+			'space': space
+		},
+		success: function(results){
+			$('#Jresult').html('');			
+			if(results.success === 1){
+				$('#Jresult').prepend('<p>'+ results.mess +'</p>');
+				$('#Jresult').prepend('<p>共'+ results.suc +'张图片裁剪成功，共'+ results.err +'张图片裁剪失败</p>');
+			}
+		},
+		error: function(){
+			alert('请稍后再试！');
+		}
+	})
+})
+
+function checkInput(val){
+	if(val && val.indexOf('，') == -1){
+		if(val.indexOf(',') > -1){
+			var vals = val.split(',');
+			if(vals.length != 2 || !parseInt(vals[0]) || !parseInt(vals[1])) {
+				return false;
+			}
+		}else if(!parseInt(val)){
+			return false;
+		}
+	}else{
+		return false;
+	}
+	return true;
+}
